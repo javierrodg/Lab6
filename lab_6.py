ac8f271841432dbb0b46cda57d5ff1716f0d19ee
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-04-24 20:33:46 -0700

    initial commit

---
+++ lab_6.py
@@ -0,0 +1,17 @@
+ 
+"""
+To start , we will generate a random intger between 1 and 20, and
+based on the result of the random number, we check to see if it falls under certain range
+of the number is greater than 15, then the result will be "cherries"
+otherwise if the number is > 10, then the result will be "orange"
+otherwise if the number is > 5, then the result will be "Plum"
+otherwise if the number is > 2, then the result will be "Melon"
+otherwise if the number is > 1, then the result will be "bell"
+if the number is not any of the above, then the result will be "Bar"
+ 
+we iterate over using a loop three times and print the results to the user. As an example "Plum Cheries"
+"""
+ 
+ 
+ 
+ 
